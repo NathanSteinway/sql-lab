2f23fd989540067aaf60c5d526a6ad1d86e77cb3
ganbari <steinway.nathan@gmail.com>
2023-06-12 14:43:28 -0500

    completes artists

---
+++ artists.sql
@@ -0,0 +1,24 @@
+-- SELECT * FROM artist;
+
+-- INSERT INTO artist (name)
+-- VALUES ('Saturdays at your place')
+
+-- INSERT INTO artist (name)
+-- VALUES ('Tyler, the Creator')
+
+-- INSERT INTO artist (name)
+-- VALUES ('Pinegrove')
+
+-- SELECT name FROM artist
+-- ORDER BY name DESC
+-- LIMIT 10;
+
+-- SELECT name FROM artist
+-- ORDER BY name ASC
+-- LIMIT 5;
+
+-- SELECT * FROM artist
+-- WHERE name LIKE 'Black%';
+
+-- SELECT * FROM artist
+-- WHERE name LIKE '%Black%';
